9b8e2eb5cdd8bfa66e07b1b83ae2dd39576fcea4
dresen <dresen@gmail.com>
2018-10-22 21:54:32 +0200

    Refactored model functionality to BaseGru

--- models/ctc_gru.py
+++ models/ctc_gru.py
@@ -5,16 +5,35 @@ import torch
 import torch.nn.functional as F
 from models.ctc_lstm import SequenceLinear
 
-class CTCgru(torch.nn.Module):
+class BaseGru(torch.nn.Module):
     def __init__(self, input_size, output_size, hidden_size, nlayers, dropout=0, bidi=False):
-        super(CTCgru, self).__init__()
+        super(BaseGru, self).__init__()
+        self.isz = input_size      # Number of features
+        self.osz = output_size
+        self.hsz = hidden_size
+        self.nlayers = nlayers
+        self.dropout = dropout
+
+        self.encoder = torch.nn.GRU(self.isz, self.hsz, dropout=self.dropout, 
+        batch_first=True, bidirectional=bidi)
+
+    def forward(self, sequences, sequence_lengths, hidden=None):
+        packed = torch.nn.utils.rnn.pack_padded_sequence(sequences, sequence_lengths, batch_first=True)
+        enc_out, _ = self.encoder(packed, hidden)
+        unpacked, _ = torch.nn.utils.rnn.pad_packed_sequence(enc_out, batch_first=True)
+        return unpacked
+
+
+class CTCgru(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(CTCgru, self).__init__(*args, **kwargs)
         self.isz = input_size      # Number of features
         self.osz = output_size + 1 # Remember to add "blank" to the output size if we use CTCLoss
         self.hsz = hidden_size
         self.nlayers = nlayers
         self.dropout = dropout
 
-        self.encoder = torch.nn.GRU(self.isz,self.hsz, dropout=self.dropout, 
+        self.encoder = torch.nn.GRU(self.isz, self.hsz, dropout=self.dropout, 
         batch_first=True, bidirectional=bidi)
 
         self.decoder = SequenceLinear(self.hsz, self.osz)
@@ -27,13 +46,15 @@ class CTCgru(torch.nn.Module):
         dec_out = self.decoder(enc_out)
         return self.logsoftmax(dec_out)
 
-        
 
-class CTCgrup(CTCgru):
+class CTCgrup(BaseGru):
     def __init__(self, *args, **kwargs):
         super(CTCgrup, self).__init__(*args, **kwargs)
         self.pack_unpack = True
+        self.osz += 1
+        self.decoder = SequenceLinear(self.hsz, self.osz)
 
+        self.logsoftmax = torch.nn.LogSoftmax(2)
 
     def forward(self, sequences, sequence_lengths):
         packed = torch.nn.utils.rnn.pack_padded_sequence(sequences, sequence_lengths, batch_first=True)
