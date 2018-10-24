d1bd74ee945704a77e65ade0abe031ce33117c8c
dresen <dresen@gmail.com>
2018-10-24 11:47:05 +0200

    Change PAD_token to -1 because it is more general

--- utils/voc.py
+++ utils/voc.py
@@ -1,5 +1,5 @@
 # Default word tokens
-PAD_token = 0  # Used for padding short sequences
+PAD_token = -1  # Used for padding short sequences
 SOS_token = 1  # Start-of-sentence token
 EOS_token = 2  # End-of-sentence token
 
