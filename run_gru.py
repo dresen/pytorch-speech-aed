201f4a3db943f21e98dcc2db50fedf708fbf90fc
dresen <dresen@gmail.com>
2018-10-24 11:43:51 +0200

    Update imports, add comment and a bugfix

--- run_gru.py
+++ run_gru.py
@@ -1,5 +1,5 @@
 """This script shows how to use the library to train speech recognisers
-end-to-end
+end-to-end with CTC loss
 """
 
 import sys
@@ -12,11 +12,14 @@ import utils.data as data
 import utils.audio as audio
 from utils.voc import generate_char_voc
 from utils.dataset import AudioDataset, Collate
-from models.ctc import CTCgrup
+from models.gru import CTCgrup
 from train import train_ctc
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
+
+# Set random seeds
+torch.manual_seed(123)
 
 audiolist = [x.strip() for x in open("/Users/akirkedal/workdir/speech/data/an4train.list").readlines()]
 reflist = [x.strip() for x in open("/Users/akirkedal/workdir/speech/data/an4train.reference").readlines()]
@@ -26,10 +29,11 @@ translist = [open(x).read().strip() for x in reflist]
 sortedlist = audio.audiosort(audiolist, list_of_references=translist)
 trainlist, trainref = zip(*sortedlist)
 
-
+# Define the voacbulary, integer maps and parition the data
 voc = generate_char_voc(trainref, "TRAIN", mode='ctc')
 partition, labels = data.format_data(trainlist, trainref, voc)
 
+# Create the torch formatted training data and data functions
 trainset = AudioDataset(partition['train'], labels, audio.mfcc)
 # Longest_first is required when we pack the input
 ctc_batch_fn = Collate(-1, longest_first=True )
@@ -40,9 +44,8 @@ params = {'batch_size': bsz,
             'collate_fn':ctc_batch_fn,
             'drop_last': True}
 
-datagenerator = tud.DataLoader(trainset, **params, )
-model = CTCgrup(40, len(voc), 120, 2, dropout=0.1, bidi=False)
-model.to(device)
+datagenerator = tud.DataLoader(trainset, **params)
+model = CTCgrup(40, len(voc), 120, 2, dropout=0.1, bidi=True)
 optimiser = torch.optim.SGD(model.parameters(),
                             lr=0.005)
 try:
