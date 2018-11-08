a6fef927cd17c85a9ccac30896a59e52729d711f
dresen <dresen@gmail.com>
2018-11-08 10:32:01 +0100

    Add dimension order, bidirectionality,  and forward returns hidden state

--- models/gru.py
+++ models/gru.py
@@ -15,25 +15,28 @@ class BaseGru(torch.nn.Module):
     Returns:
         BaseGru -- A GRU-based recurrent net
     """
-    def __init__(self, input_size, hidden_size, nlayers, dropout=0, bidi=False):
+    def __init__(self, input_size, hidden_size, nlayers, dropout=0, bidi=False, batch_first=True):
         super(BaseGru, self).__init__()
         self.isz = input_size      # Number of features
         self.hsz = hidden_size
         self.nlayers = nlayers
         self.dropout = dropout
         self.directions = 2 if bidi else 1
+        self.batch_first=batch_first
 
         self.encoder = torch.nn.GRU(self.isz, self.hsz, self.nlayers, dropout=self.dropout, 
         batch_first=True, bidirectional=bidi)
 
     def forward(self, sequences, sequence_lengths, hidden=None):
-        packed = torch.nn.utils.rnn.pack_padded_sequence(sequences, sequence_lengths, batch_first=True)
-        enc_out, _ = self.encoder(packed, hidden)
-        unpacked, _ = torch.nn.utils.rnn.pad_packed_sequence(enc_out, batch_first=True)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(sequences, sequence_lengths,
+                                                         batch_first=self.batch_first)
+        enc_out, new_hidden_state = self.encoder(packed, hidden)
+        unpacked, _ = torch.nn.utils.rnn.pad_packed_sequence(enc_out,
+                                                                batch_first=True)
         if self.directions == 2:
             # Sum both directions
             unpacked = unpacked[:,:,:self.hsz] + unpacked[:,:,self.hsz:]
-        return unpacked
+        return unpacked, new_hidden_state
 
 
 class CTCgru(torch.nn.Module):
@@ -84,7 +87,7 @@ class CTCgrup(BaseGru):
         unpacked, _ = torch.nn.utils.rnn.pad_packed_sequence(enc_out, batch_first=True)
         unpacked, _ = torch.nn.utils.rnn.pad_packed_sequence(enc_out, batch_first=True)
         if self.directions == 2:
-            # Sum both directions
+            # Sum both directions so we get the right dimensions
             unpacked = unpacked[:,:,:self.hsz] + unpacked[:,:,self.hsz:]
         dec_out = self.decoder(unpacked)
         return self.logsoftmax(dec_out)
